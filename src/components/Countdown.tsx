9372e1bd543aea581ca3b206f11bbcd9a2e009a9
Aslak Hollund <aslakhol@gmail.com>
2022-09-11 18:46:09 +0200

    better clock

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -7,12 +7,31 @@ const Countdown = (props: CountdownProps) => {
 
   const diff = goTime.getTime() - now.getTime();
 
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor(diff / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  if (days > 1) {
+    return (
+      <span className="break-inside-avoid text-7xl bold py-5 text-center">
+        {days} day{days === 1 ? "" : "s"}
+      </span>
+    );
+  }
+
+  if (hours > 1) {
+    return (
+      <span className="break-inside-avoid text-7xl bold py-5 text-center">
+        {hours} hour{hours === 1 ? "" : "s"} {minutes} minute
+        {minutes === 1 ? "" : "s"}
+      </span>
+    );
+  }
 
   return (
-    <span className="break-inside-avoid text-7xl bold pt-4 pb-10 text-center">
-      {hours} hours {minutes} minutes
+    <span className="break-inside-avoid text-7xl bold py-5 text-center">
+      {minutes}:{seconds}
     </span>
   );
 };
