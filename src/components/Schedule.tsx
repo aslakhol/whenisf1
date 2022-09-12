55626e749b7d09f1345044ed867589b212af16bf
Aslak Hollund <aslakhol@gmail.com>
2022-09-12 08:09:27 +0200

    kill the toggle idea

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,31 +1,23 @@
-// import { useState } from "react";
 import type monza from "../monza.json";
 import RaceTableEntry from "./RaceTableEntry";
 import RaceTableHeadings from "./RaceTableHeadings";
-// import Toggle from "./Toggle";
 
 type Race = typeof monza.MRData.RaceTable.Races[0];
 type ScheduleProps = { races: Race[] };
 
 const Schedule = (props: ScheduleProps) => {
   const { races } = props;
-  // const [displayLocalTime, setDisplayLocalTime] = useState(true);
 
   return (
     <>
-      {/* <Toggle
-        label="Display times in Local Timezone (or utc)"
-        checked={displayLocalTime}
-        onChange={setDisplayLocalTime}
-      /> */}
-
+      <p>The times are shown in your local timezone.</p>
       <table className="min-w-1/2 divide-y divide-gray-300">
         <thead className="bg-gray-50">
           <RaceTableHeadings />
         </thead>
         <tbody className="divide-y divide-gray-200 bg-white">
           {races.map((race) => (
-            <RaceTableEntry race={race} utc={true} />
+            <RaceTableEntry race={race} utc={false} />
           ))}
         </tbody>
       </table>
