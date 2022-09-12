6fd5c6653704791053df2f3b9796d2a52ec5c882
Aslak Hollund <aslakhol@gmail.com>
2022-09-12 08:25:22 +0200

    mark passed times

--- src/components/RaceTableEntry.tsx
+++ src/components/RaceTableEntry.tsx
@@ -25,6 +25,8 @@ interface Props {
 const RaceTableEntry = (props: Props) => {
   const { race, utc } = props;
 
+  const now = new Date();
+
   const circuit = race.Circuit.Location.locality;
   const fp1 = new Date(race.FirstPractice.date + "T" + race.FirstPractice.time);
   const fp2 = new Date(
@@ -45,22 +47,46 @@ const RaceTableEntry = (props: Props) => {
       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
         {circuit}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          raceDate > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceDate goTime={raceDate} utc={utc} />
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          fp1 > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceTime goTime={fp1} utc={utc} />
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          fp2 > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceTime goTime={fp2} utc={utc} />
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          fp3orSprint > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceTime goTime={fp3orSprint} utc={utc} />
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          qualifying > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceTime goTime={qualifying} utc={utc} />
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={`whitespace-nowrap px-3 py-4 text-sm ${
+          raceDate > now ? "text-gray-600" : "text-gray-400"
+        }`}
+      >
         <RaceTime goTime={raceDate} utc={utc} />
       </td>
     </tr>
