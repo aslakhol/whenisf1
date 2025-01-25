daa96a15e370ae443c862c3258ce8489c144356a
Aslak Hollund <aslakhol@gmail.com>
2025-01-25 22:32:04 +0100

    replace splitbee and vercel analytics with posthog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
@@ -8,7 +7,4 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
